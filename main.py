5fb99af31c78742ffd67e6e73c46b069ee817f2d
Evgeny905 <gorbatkovei@gmail.com>
2024-06-11 21:30:26 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+print(len('56765757556'))             #a=56765757556
+print(5+3,5-3)                        #first=5, second=3, summa=8, diff=2
+print((2+9+1)//3)                     #first=2, second=9, third=1, mean=4
+print('Понедельник',', ','Вторник')   #first_string=Понедельник, second_string=Вторник
+print((((8*6+8*4)**3))//2)            #a=8, b=6, c=4, f=80
